d4e487fffe8e25ac748511937314137952b03306
iandelong <iandelong1@gmail.com>
2024-04-30 20:27:37 -0700

    feat: intro done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,22 @@
+import Link from 'next/link';
 import Header from '@/app/components/header';
 
 const Home: React.FC = () => {
   return (
-    <div>
+    <div className="font-oswald">
       <Header/>
-      <h1>-</h1>
+      <div className="ml-48 mt-40 flex flex-col">
+        <span className="w-1/3 text-4xl font-semibold">Hey, I&apos;m Ian - A Software Engineer in Seattle</span>
+        <div className="w-2/5 mt-10 text-xl font-light">I&apos;ve crafted interactive solutions for production scheduling at Smiling Woods Yurts, developed full-stack applications for Garmin devices at PowerEQN Inc, and I’m currently looking for my next role.  </div>
+        <Link className="mr-auto mt-5 text-xl font-normal hover:text-gray-700 hover:border-slate-600 border-solid border-b border-slate-900" href="mailto:iandelong1@gmail.com" target="_blank">iandelong1@gmail.com</Link>
+      </div>
+      <div className="ml-48 mt-24">
+        <div className="text-4xl font-semibold">
+          Featured Work
+        </div>
+        
+        
+      </div>
     </div>
   );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,7 @@ const config: Config = {
     extend: {
       fontFamily: {
         montserrat: ["Montserrat", "sans-serif"],
+        oswald: ["Oswald"],
       },
     },
   },
